a7c798df8ba57daaa4d220112656bfd2905ca632
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-03-01 12:56:29 +0400

    fix mistakes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import { head, adds, chouse } from "../animation/animation.js";
 
 const header = document.querySelector(".header__svg");
+
 const headerImg = document.querySelector(".header__img");
 const chousingUs = document.querySelector(".chousing__us");
 
 header.addEventListener("click", head);
 headerImg.addEventListener("click", adds);
 chousingUs.addEventListener("click", chouse);
-export { header,headerImg ,chousingUs};
-
+export { header, headerImg, chousingUs };
