1d92d4e955f053ca294c11db3227723be26eb262
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-03-07 12:19:55 +0400

    add animation

--- animation/animation.js
+++ animation/animation.js
@@ -1,4 +1,4 @@
-import { header,headerImg ,chousingUs} from "../script.js";
+import { header,headerImg ,chousingUs,product} from "../script.js";
 const head = () => {
     header.classList.toggle("magazin");
  }
@@ -8,5 +8,7 @@ const head = () => {
  const chouse = () => {
      chousingUs.classList.toggle("magazin");
  }
- 
- export { head, adds, chouse };
+ const prod = () =>{
+    product.classList.toggle("magazin");
+ }
+ export { head, adds, chouse ,prod };
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-import { head, adds, chouse } from "../animation/animation.js";
+import { head, adds, chouse ,prod } from "../animation/animation.js";
 
 const header = document.querySelector(".header__svg");
 
 const headerImg = document.querySelector(".header__img");
 const chousingUs = document.querySelector(".chousing__us");
+const product = document.querySelector(".product h1");
 
 header.addEventListener("click", head);
 headerImg.addEventListener("click", adds);
 chousingUs.addEventListener("click", chouse);
-export { header, headerImg, chousingUs };
+product.addEventListener("click",prod)
+
+export { header, headerImg, chousingUs ,product };
